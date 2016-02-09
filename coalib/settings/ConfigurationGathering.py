84eda5787dd419008e0a85ff2f672355b40a854b
AbdealiJK <abdealikothari@gmail.com>
2016-02-09 11:38:37 +0530

    ConfigurationGathering: Remove mutable argument
    ConfigurationGathering has a mutable argument which is causing
    trouble with pytest being able to modify the arguments there.
    This sets that value to None and replaces it inside the function.

--- coalib/settings/ConfigurationGathering.py
+++ coalib/settings/ConfigurationGathering.py
@@ -191,7 +191,7 @@ def find_user_config(file_path, max_trials=10):
     return ""
 
 
-def gather_configuration(acquire_settings, log_printer, arg_list=sys.argv[1:]):
+def gather_configuration(acquire_settings, log_printer, arg_list=None):
     """
     Loads all configuration files, retrieves bears and all needed
     settings, saves back if needed and warns about non-existent targets.
@@ -223,6 +223,7 @@ def gather_configuration(acquire_settings, log_printer, arg_list=sys.argv[1:]):
                                 section
                               * The targets list
     """
+    arg_list = sys.argv[1:] if arg_list == None else arg_list
     sections, targets = load_configuration(arg_list, log_printer)
     local_bears, global_bears = fill_settings(sections,
                                               acquire_settings,
